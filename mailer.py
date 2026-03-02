b0fe9a09d43f4def4719f62c73c3c90a4bd12291
Om Sharma <155080781+Omvats0@users.noreply.github.com>
2026-03-02 22:49:13 +0530

    Add files via upload

---
+++ mailer.py
@@ -0,0 +1,55 @@
+
+import gmail
+
+#credentials
+email="oms87742@gmail.com"
+app_pass="hroo uyep iwwe airb"
+
+def send_openacn_email(uemail,uacn,upass,uname):
+    con=gmail.GMail(email,app_pass)
+    text_msg=f'''Welcome{uname},
+    You have successfully opened your account with our bank ABC
+    Here is your credentials
+    ACN = {uacn}
+    pass = {upass}
+    
+    Kindly Change your password on 1st login
+    
+    Thanks
+    ABC Bank
+    Ghaziabad,UP,India
+    '''
+    msg=gmail.Message(to=uemail,subject="Account Opened",text=text_msg)
+    con.send(msg)
+
+
+def send_otp_forgot(uemail,uname,otp):
+    con=gmail.GMail(email,app_pass)
+    text_msg=f'''Hello {uname},
+Here is your otp to recover password
+OTP = {otp}
+    
+Kindly do not share otp to others 
+    
+Thanks
+ABC Bank
+Ghaziabad,UP,India
+'''
+    msg=gmail.Message(to=uemail,subject="Passwword Recovery",text=text_msg)
+    con.send(msg)
+
+
+def send_otp_close(uemail,uname,otp):
+    con=gmail.GMail(email,app_pass)
+    text_msg=f'''Hello {uname},
+Here is your otp to close your account
+OTP = {otp}
+    
+Kindly do not share otp to others
+    
+Thanks
+ABC Bank
+Ghaziabad,UP,India
+'''
+    msg=gmail.Message(to=uemail,subject="Passwword Recovery",text=text_msg)
+    con.send(msg)
